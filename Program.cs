c56166f11ff5dbaec01c57926f8a73ce5707d11d
Joackson Santos <joackson3@gmail.com>
2022-02-15 15:24:43 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int n , horas, minutos,segundos;
+n= int.Parse(Console.ReadLine());
+
+if(n >= 60){
+   segundos = n % 60;
+   n = n / 60;
+}else{
+    segundos = n;
+    n =0;
+}
+
+if(n < 60){
+    minutos = n;
+    n =0;
+}else{
+    minutos = n % 60;
+    n = n / 60;
+}
+
+if(n < 60){
+    horas = n;
+    n =0;
+}else{
+    horas = n % 60;
+    n = n / 60;
+}
+
+Console.WriteLine(horas+":"+minutos+":"+segundos);
